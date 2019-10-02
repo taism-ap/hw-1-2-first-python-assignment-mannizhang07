c9543f16fb121c30f7514016baca920ef5a1ee93
mannizhang07 <54617730+mannizhang07@users.noreply.github.com>
2019-10-02 10:53:34 +0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,17 +1,18 @@
-print ("This is a simple calculator that only takes two values and can preform 4 functions")
-num1 = float(input("Please enter your first number")
-             
-op = input("Please select the appropriate symbols for the operation you would like perfromed, - is subtract, + is add, * is mutiply, and / is divide")
-             
-num2= float(input("Please enter your second number")
+print("This is a simple calculator that only takes two values and can preform 4 functions")
+num1= input("Please enter your first number: ")
 
-if op == "-"
-          print(num1-num2)
-if op == "+"
-          print(num1+num2)
-if op == "*"
-          print(num1*num2)
-if op == "/"
-          print(num1/num2)
+op= input("Please select the appropriate symbols for the operation you would like perfromed, - is subtract, + is add, * is mutiply, and / is divide")
+
+num2= input("Please enter your second number: ")
+
+
+if op == "-":
+          print(float(num1)-float(num2))
+if op == "+":
+          print(float(num1)+float(num2))
+if op == "*":
+          print(float(num1)*float(num2))
+if op == "/":
+          print(float(num1)/float(num2))
 
 

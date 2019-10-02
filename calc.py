6e8facac8257cd1d137e4b5791f7ab28c6978e4f
mannizhang07 <54617730+mannizhang07@users.noreply.github.com>
2019-10-02 10:10:53 +0400

    Update and rename README.md to calc.py

---
+++ calc.py
@@ -0,0 +1,17 @@
+print ("This is a simple calculator that only takes two values and can preform 4 functions")
+num1 = float(input("Please enter your first number")
+             
+op = input("Please select the appropriate symbols for the operation you would like perfromed, - is subtract, + is add, * is mutiply, and / is divide")
+             
+num2= float(input("Please enter your second number")
+
+if op == "-"
+          print(num1-num2)
+if op == "+"
+          print(num1+num2)
+if op == "*"
+          print(num1*num2)
+if op == "/"
+          print(num1/num2)
+
+
